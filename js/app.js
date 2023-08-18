543576d0e737679575bf6f0a2ce138f8f328363d
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2023-08-18 23:26:01 +0300

    Update 2023-08-18T20:25:59.756Z

--- js/app.js
+++ js/app.js
@@ -76,7 +76,7 @@ function menuToggle() {
 		social.style.opacity = 0;
 		logo.style.opacity = 0;
 		setTimeout(() => {
-			headerFixed.classList.add("noborder");
+			headerFixed.classList.add("header_noborder");
 		}, 300);
 	} else {
 		// скрыть меню
@@ -84,7 +84,7 @@ function menuToggle() {
 		social.style.opacity = 1;
 		logo.style.opacity = 1;
 		setTimeout(() => {
-			headerFixed.classList.remove("noborder");
+			headerFixed.classList.remove("header_noborder");
 		}, 200);
 	}
 
